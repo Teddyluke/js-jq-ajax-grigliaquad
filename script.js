f3f74d0e807cde0a793e01d1a705a7283a14fbc5
Luca Maglie <luca.maglie2@gmail.com>
2020-07-21 15:12:55 +0200

    esercizio del giorno.

---
+++ script.js
@@ -0,0 +1,52 @@
+// al click su ogni sezione succede qualcosa
+
+function addClickListener() {
+var sezioni = $(".sezione");
+sezioni.click(clickOnSection);
+};
+
+function clickOnSection() {
+var sezione = $(this);
+$.ajax(
+{
+url: "https://flynn.boolean.careers/exercises/api/random/int",
+method: "GET",
+success: function (data, stato) {
+  var success = data["success"];
+  var value = data["response"];
+console.log(value);
+  if (success) {
+    if (value <= 5) {
+      sezione.addClass("yellow");
+      sezione.append(value);
+    } else {
+      sezione.addClass("green");
+      sezione.append(value);
+    }
+  }
+},
+error: function (richiesta, stato, errori) {
+}
+
+
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init() {
+addClickListener()
+
+};
+
+$(document).ready(init);
